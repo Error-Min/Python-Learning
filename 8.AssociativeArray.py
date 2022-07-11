373207ad678dff3be2f65eabe8f0cb9154be958f
Error-Min <smgm030@gmail.com>
2022-07-11 17:20:22 +0900

    220711 code

---
+++ 8.AssociativeArray.py
@@ -0,0 +1,44 @@
+# dic = {'name':'Errormin', 
+#        'phone':'1011212334', 
+#        'birth':'12123'}
+
+# print(dic)
+
+
+
+# a = {1: 'hi'}
+# a[2] = 'by'
+# a['name'] = 'ErrorMin'
+
+# print(a)
+
+# del a['name']
+
+# print(a)
+
+dic = {'name': 'ErrorMin',
+       'age' : 27,
+       'phone' : 121231231}
+
+# print(dic.values())
+
+# for k in dic.values():
+#     print(k)
+
+# print(list[dic.keys()])
+
+# print(list[dic.values()])
+
+# print(dic.items())
+
+# dic.clear()
+# print(dic)
+
+# print(dic.get('name'))
+
+# print(dic.get('phone'))
+
+print('name' in dic)
+print('email' in dic)
+
+
--- Python_Learnings/List.py
+++ Python_Learnings/List.py
@@ -1,8 +1,14 @@
-list = [1, 2, 3, 4, 5]
+# list = [1, 2, 3, 4, 5]
 
-print(list[:3])
+# print(list[:3])
 
 
-list2 = [12, 13, ['errormin', 'sangmin']]
+# list2 = [12, 13, ['errormin', 'sangmin']]
 
-print(list2[0:][0])
+# print(list2[2][0])
+
+List4 = [1, 2, 3, 4, 5]
+
+List4[2] = 8
+
+print(List4)
