0ace77f3a3c3e388481f669126b3ca4376837654
Error-Min <smgm030@gmail.com>
2022-07-14 17:10:43 +0900

    tt

---
+++ cless.py
@@ -0,0 +1,20 @@
+class Sftware:
+    def __init__(self, age, name, name2):
+        self.age = age
+        self.name = name
+        self.name2 = name2
+    
+    def print_name(self):
+        self.age
+    
+    def print_age(self):
+        self.name
+    
+    def print_name2(self):
+        self.name2
+        
+a = Sftware(17 , "sangmin", "errormin")
+
+print(a.age)
+print(a.name)
+print(a.name2)
