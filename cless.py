7882eb8892c75211177b40d85037338436e9cbb9
Error-Min <smgm030@gmail.com>
2022-07-20 10:40:09 +0900

    tta

--- cless.py
+++ cless.py
@@ -18,3 +18,21 @@ a = Sftware(17 , "sangmin", "errormin")
 print(a.age)
 print(a.name)
 print(a.name2)
+
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a-b
+
+if __name__ == "__main__" :
+    print(add(1, 2))
+    print(sub(1, 2))
+
+
+
+
+
+
+
